ee811cbe6a66054e8e0ffa8038673ac76932db41
99ahsanahmed <ahsan.ha58@gmail.com>
2024-11-01 21:23:34 +0500

    express crud app completed!

--- 10_backend/02_express/index.js
+++ 10_backend/02_express/index.js
@@ -1,6 +1,7 @@
-const express = require("express")
+require("dotenv").config();
+const express = require("express");
 const app = express();
-
+const port = process.env.PORT || 3000;
 app.use(express.json());
 
 let userData = [];
@@ -8,50 +9,48 @@ let userId = 1;
 
 //getting data
 app.post("/users", (req, res) => {
-  const {name , rollno} = req.body;
-  const newUser = {id : userId++ , name, rollno}
-  userData.push(newUser)
-  res.status(200).send(newUser)
+  const { name, rollno } = req.body;
+  const newUser = { id: userId++, name, rollno };
+  userData.push(newUser);
+  res.status(200).send(newUser);
 });
 
 //printing data
-app.get("/users" , (req,res)=>{
+app.get("/users", (req, res) => {
   res.status(200).send(userData);
 });
 
 //getting specific data through id
-app.get("/users/:id", (req,res)=>{
-  const user = userData.find(t=>t.id === parseInt(req.params.id) )
-  if(!user){
-    return res.status(404).send("User not found")
+app.get("/users/:id", (req, res) => {
+  const user = userData.find((t) => t.id === parseInt(req.params.id));
+  if (!user) {
+    return res.status(404).send("User not found");
   }
   res.status(200).send(user);
-
-})
+});
 
 //To Update
-app.put("/users/:id",(req,res)=>{
-  const user = userData.find(t=>t.id === parseInt(req.params.id) )  
-  if(!user){
-    return res.status(404).send("User not found")
+app.put("/users/:id", (req, res) => {
+  const user = userData.find((t) => t.id === parseInt(req.params.id));
+  if (!user) {
+    return res.status(404).send("User not found");
   }
-  const {name,rollno} = req.body
-  user.name = name,
-  user.price = price
+  const { name, rollno } = req.body;
+  (user.name = name), (user.rollno = rollno);
   res.status(200).send(user);
-})
+});
 
 //Delete Method
-app.delete("/users/:id",(req,res)=>{
-  const index = userData.findIndex(t => t === parseInt(req.params.id))
-  if(index < 0 ){
-    return res.status(404).send("invalid input")
+app.delete("/users/:id", (req, res) => {
+  const index = userData.findIndex((t) => t.id === parseInt(req.params.id));
+  console.log(index);
+  if (index === -1) {
+    return res.status(404).send("invalid input");
   }
-  userData.splice(index,1)
-  return res.status(204).send("Deleted Succesfully");
-})
+  userData.splice(index, 1);
+  res.status(200).send("Deleted Succesfully");
+});
 
-
-app.listen(3000, () => {
-  console.log(`server is listening at port 3000`);
+app.listen(port, () => {
+  console.log(`server is listening at port ${port}`);
 });
